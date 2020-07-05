e5d3ac51dc189442c3a4db462924a7260fb291ef
Muhammet Kaya <muhammetkaya3509@hotmail.com>
2020-07-05 11:24:20 +0300

    integrated Redis cache for session state.

---
+++ dist-session-management/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace dist_session_management.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string newSessionValue = "testValue";
+            string sessionKey = "testKey";
+
+            HttpContext.Session.SetString(sessionKey, newSessionValue);
+        }
+    }
+}
